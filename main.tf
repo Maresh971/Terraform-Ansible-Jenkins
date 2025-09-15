9c84190e509be8a1abcffac2d9ade3931ecaae73
Maresh971 <136575570+Maresh971@users.noreply.github.com>
2025-09-15 19:19:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,22 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0360c520857e3138f" # us-east-1
+  ami           = "ami-0360c520857e3138f" # Ubuntu 22.04 in us-east-1
   instance_type = "t2.medium"
-  tags = {
-      Name = "TF-Instance"
-  }
-}
 
-  # Optionally output public IP for Ansible
+  tags = {
+    Name = "TF-Instance"
+  }
+
+  # Save public IP to file for Ansible
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > ec2_ip.txt"
   }
+}
+
+# Output public IP for Jenkins
+output "public_ip" {
+  value = aws_instance.foo.public_ip
 }
